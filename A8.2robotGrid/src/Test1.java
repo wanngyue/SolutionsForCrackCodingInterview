7f0f38e651837c0529b7aa6dc122a3b36ae9a905
wangyue <wyue.leon@gmail.com>
2014-03-11 13:21:58 +0100

    Ex8.2
    Imagine a robot sitting on the upper left hand corner of an NxN grid.
    The robot can only move in two directions: right and down. How many
    possible paths are there for the robot?
    FOLLOW UP
    Imagine certain squares are “off limits”, such that the robot can not
    step on them. Design an algorithm to get all possible paths for the
    robot.

---
+++ A8.2robotGrid/src/Test1.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int size = 1;
+		System.out.println("The number of the possible paths in a " + size + "*" + size + 
+				" is "+ getNumOfPaths(size, size));
+		size = 2;
+		System.out.println("The number of the possible paths in a " + size + "*" + size + 
+				" is "+ getNumOfPaths(size, size));
+		size = 3;
+		System.out.println("The number of the possible paths in a " + size + "*" + size + 
+				" is "+ getNumOfPaths(size, size));
+		size = 4;
+		System.out.println("The number of the possible paths in a " + size + "*" + size + 
+				" is "+ getNumOfPaths(size, size));
+	}
+	
+	
+	public static int getNumOfPaths(int sizeX, int sizeY){
+		if(sizeX < 1 || sizeY < 1){
+			return Integer.MIN_VALUE;
+		}
+		return sum(sizeX - 1 + sizeY - 1)/(sum(sizeX - 1)*sum(sizeY - 1));
+	}
+	public static int sum(int value){
+		if(value > 1){
+			return value * sum(value - 1);
+		}else {
+			return 1;
+		}
+	}
+}
